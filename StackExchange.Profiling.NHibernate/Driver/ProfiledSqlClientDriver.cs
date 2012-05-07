1b83bcadaa2a7ac83547b7a4bb454429e39475c0
Oleg Kosmakov <kosmakoff@gmail.com>
2012-05-07 13:38:59 +0300

    Added NHibernate support

---
+++ StackExchange.Profiling.NHibernate/Driver/ProfiledSqlClientDriver.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.Common;
+using NHibernate.Driver;
+
+namespace StackExchange.Profiling.NHibernate.Driver
+{
+    public class ProfiledSqlClientDriver : SqlClientDriver
+    {
+        public override System.Data.IDbCommand CreateCommand()
+        {
+            IDbCommand command = base.CreateCommand();
+
+            if (MiniProfiler.Current != null)
+                command = new ProfiledSqlCommand((DbCommand)command, MiniProfiler.Current);
+
+            return command;
+        }
+    }
+}
---
+++ StackExchange.Profiling.NHibernate/ProfiledSqlCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using StackExchange.Profiling.Data;
+
+namespace StackExchange.Profiling.NHibernate
+{
+    public class ProfiledSqlCommand : ProfiledDbCommand
+    {
+        private readonly SqlCommand _sqlCommand;
+
+        public ProfiledSqlCommand(DbCommand command, IDbProfiler profiler)
+            : base(command, null, profiler)
+        {
+            _sqlCommand = (SqlCommand)command;
+        }
+
+        public SqlCommand SqlCommand
+        {
+            get { return _sqlCommand; }
+        }
+    }
+}
